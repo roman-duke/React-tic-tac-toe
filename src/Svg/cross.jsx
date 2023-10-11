e963744b6326fa795edc491bc960d02440c31a39
Donald Roman <romanunuose@gmail.com>
2023-10-11 23:49:36 +0100

    Refactord the SVG code for cross and nought into separate components

---
+++ src/Svg/cross.jsx
@@ -0,0 +1,7 @@
+export default function Cross() {
+  return (
+    <svg id="cross" viewBox="0 0 24 24">
+      <path d="M6 18L18 6M6 6l12 12" />
+    </svg>
+  )
+}
---
+++ src/Svg/nought.jsx
@@ -0,0 +1,7 @@
+export default function Nought() {
+  return ( 
+    <svg id="nought" viewBox="0 0 24 24">
+      <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"/>
+    </svg>
+  )
+}
