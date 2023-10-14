9c3125e2f6e407606e11e773dd236a8fb0711519
Donald Roman <romanunuose@gmail.com>
2023-10-14 09:49:44 +0100

    Modified the TickIcon component

--- src/Svg/tick.jsx
+++ src/Svg/tick.jsx
@@ -1,6 +1,6 @@
 export default function TickIcon() {
   return ( 
-    <svg viewBox="0 0 24 24" fill="none">
+    <svg id="tick" viewBox="0 0 24 24" fill="none">
       <path d="M18 7L9.42857 17L6 13" stroke="#363853" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
