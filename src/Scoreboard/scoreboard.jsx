2c70abf5b271e4ef5cff5f3f6d54f428a81a9e75
Donald Roman <romanunuose@gmail.com>
2023-10-14 15:33:18 +0100

    Made the scoreboard component stop showing player to move indicator if there is a draw

--- src/Scoreboard/scoreboard.jsx
+++ src/Scoreboard/scoreboard.jsx
@@ -4,23 +4,25 @@ import Nought from '../Svg/nought.jsx'
 import './score.scss'
 
 ScoreBoard.propTypes = {
+  computerToStart: PropTypes.func.isRequired,
   xIsNext: PropTypes.bool.isRequired,
+  noDraw: PropTypes.bool.isRequired,
   playerOneScore: PropTypes.oneOfType([PropTypes.number, PropTypes.object]),
   playerTwoScore: PropTypes.oneOfType([PropTypes.number, PropTypes.object])
 }
 
-export default function ScoreBoard({xIsNext, playerOneScore, playerTwoScore}) {
+export default function ScoreBoard({noDraw, computerToStart, xIsNext, playerOneScore, playerTwoScore}) {
 
   return (
     <div className="score-bar">
-      <button className={`${xIsNext ? 'active' : ''} left-bar`}>
+      <button className={`${xIsNext ? (noDraw ? 'active' : '') : ''} left-bar`}>
         <span className="cross">
           <Cross />
         </span>
         <span className="score">{playerOneScore}</span>
       </button>
 
-      <button className={`${xIsNext ? '' : 'active'} right-bar`}>
+      <button onClick={() => computerToStart()} className={`${xIsNext ? (noDraw ? '' : '') : 'active'} right-bar`}>
         <span className="nought">
           <Nought />
         </span>
