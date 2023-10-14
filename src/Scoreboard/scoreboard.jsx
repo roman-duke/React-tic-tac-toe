678238692b330c438c54d326ff2d50bba51e8d73
Donald Roman <romanunuose@gmail.com>
2023-10-14 09:38:48 +0100

    Lifted state up to Parent component

--- src/Scoreboard/scoreboard.jsx
+++ src/Scoreboard/scoreboard.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import PropTypes from 'prop-types'
 import Cross from '../Svg/cross'
 import Nought from '../Svg/nought.jsx'
@@ -6,31 +5,11 @@ import './score.scss'
 
 ScoreBoard.propTypes = {
   xIsNext: PropTypes.bool.isRequired,
-  gameWin: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  playerOneScore: PropTypes.oneOfType([PropTypes.number, PropTypes.object]),
+  playerTwoScore: PropTypes.oneOfType([PropTypes.number, PropTypes.object])
 }
 
-export default function ScoreBoard({gameWin, xIsNext}) {
-  const [scores, setScores] = useState([null, null]);
-  const [isScoreUpdated, setIsScoreUpdated] = useState(false);
-
-  const cross_score = scores[0] !== null ? scores[0] : <>&ndash;</>;
-  const nought_score = scores[1] !== null ? scores[1] : <>&ndash;</>;
-
-  function scoreUpdate() {
-    const nextScores = scores.slice();
-    if (xIsNext) {
-      nextScores[1] += 1;
-      setScores(nextScores);
-    } else {
-      nextScores[0] += 1;
-      setScores(nextScores);
-    }
-  }
-
-  if (gameWin && isScoreUpdated == false) {
-    scoreUpdate();
-    setIsScoreUpdated(true);
-  }
+export default function ScoreBoard({xIsNext, playerOneScore, playerTwoScore}) {
 
   return (
     <div className="score-bar">
@@ -38,14 +17,14 @@ export default function ScoreBoard({gameWin, xIsNext}) {
         <span className="cross">
           <Cross />
         </span>
-        <span className="score">{cross_score}</span>
+        <span className="score">{playerOneScore}</span>
       </button>
 
       <button className={`${xIsNext ? '' : 'active'} right-bar`}>
         <span className="nought">
           <Nought />
         </span>
-        <span className="score">{nought_score}</span>
+        <span className="score">{playerTwoScore}</span>
       </button>
     </div>
   )
